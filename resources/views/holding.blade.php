3f8f9e48e57d2cad9d319b34053df8289cf0bb56
lukeramsden <lukeramsden@protonmail.com>
2018-03-13 10:54:13 +0000

    lang

--- resources/views/holding.blade.php
+++ resources/views/holding.blade.php
@@ -15,12 +15,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <meta name="description" content="Welcome to Career In Health. Innovative recruitment built exclusively for the UK's health sector">
-
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <title>{{ config('app.name', 'Laravel') }}</title>
         <link rel="shortcut icon" type="image/png" href="/icons/favicon.png"/>
+        <link rel="alternate" href="{{ env('APP_URL') }}" hreflang="en-gb" />
+
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
