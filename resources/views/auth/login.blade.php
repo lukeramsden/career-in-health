2422882b491310672d62c82455b912e6673c8ea8
lukeramsden <lukeramsden@protonmail.com>
2018-03-20 10:52:50 +0000

    :thinking:

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
 
                          <div class='row'>
                             <div class='col-12'>
-                                <h1>Login to Career In Care</h1>
+                                <h1>Login to Career In Health</h1>
                             </div>
 
                             <div class='col-12 mt-5'>
