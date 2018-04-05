752a9938b6b7cc9d490c8d26943c529c201839ab
lukeramsden <lukeramsden@protonmail.com>
2018-04-05 15:23:11 +0100

    updated login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,41 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container align-self-center">
-        <form method="POST" action="{{ route('login') }}" class="m-5">
-            {{ csrf_field() }}
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-auto" id="login-container">
+                <div class="card card-custom">
+                    <div class="card-body">
+                        <form action="{{ route('login') }}" method="post">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label>Email Address (<span class='text-action'>*</span>)</label>
+                                <input type="email" name='email' class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                                    placeholder="Email Address" value='{{ old('email') }}'>
 
-             <div class='row'>
-                <div class='col-12'>
-                    <h1>Login to Career In Health</h1>
-                </div>
+                                @if ($errors->has('email'))
+                                    <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+                                @endif
+                            </div>
 
-                <div class='col-12 mt-5'>
-                    <div class="form-group">
-                        <label>Email Address (<span class='text-action'>*</span>)</label>
-                        <input type="email" name='email' class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                            placeholder="Email Address" value='{{ old('email') }}'>
+                            <div class="form-group">
+                                <label>Password (<span class='text-action'>*</span>)</label>
+                                <input type="password" name='password'
+                                    class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
+                                    placeholder="Password">
 
-                        @if ($errors->has('email'))
-                            <div class="invalid-feedback">{{ $errors->first('email') }}</div>
-                        @endif
-                    </div>
-                </div>
+                                @if ($errors->has('password'))
+                                    <div class="invalid-feedback">{{ $errors->first('password') }}</div>
+                                @endif
+                            </div>
 
-                <div class='col-md-12 mt-3'>
-                    <div class="form-group">
-                        <label>Password (<span class='text-action'>*</span>)</label>
-                        <input type="password" name='password'
-                            class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
-                            placeholder="Password">
-
-                        @if ($errors->has('password'))
-                            <div class="invalid-feedback">{{ $errors->first('password') }}</div>
-                        @endif
-                    </div>
-                </div>
-
-                <div class='col-md-12 mt-3'>
-                    <div class="form-group">
-                        <button class='btn btn-action'>Login</button>
+                            <div class="form-group">
+                                <button class='btn btn-action'>Login</button>
+                            </div>
+                            
+                        </form>
                     </div>
                 </div>
             </div>
-        </form>
-        
+        </div>
     </div>
 @endsection
