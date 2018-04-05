39453a594fd00a0b00822326798ad37c5d5cc997
lukeramsden <lukeramsden@protonmail.com>
2018-04-05 11:56:42 +0100

    altered layouts hiearchy, added login view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,73 +1,52 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        @if (env('APP_ENV') != 'local')
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109407615-2"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'UA-109407615-2');
-            </script>
-        @endif
-        
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="shortcut icon" type="image/png" href="/images/favicon.png"/>
-
-        <link href="/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        @yield('stylesheet')
-    </head>
-    <body>
-        <div id="navbar">
-            <img class="logo" src="/images/cih-logo.svg" alt="logo">
-            <div id="nav-inner">
-                <nav class="nav flex-column">
-                    @guest
-                        <a class="nav-link {{ active_route('welcome') }}" href="{{ route('welcome') }}">Home</a>
-                        <a class="nav-link {{ active_route('search') }}" href="{{ route('search') }}">Search</a>
-                    @endguest
+@extends('layouts.base')
+@section('b_content')
+    <div id="navbar">
+        <img class="logo" src="/images/cih-logo.svg" alt="logo">
+        <div id="nav-inner">
+            <nav class="nav flex-column">
+                @guest
+                    <a class="nav-link {{ active_route('welcome') }}" href="{{ route('welcome') }}">Home</a>
+                    <a class="nav-link {{ active_route('search') }}" href="{{ route('search') }}">Search</a>
+                    <a class="nav-link {{ active_route('login') }}" href="{{ route('login') }}">Log In</a>
+                    <a class="nav-link {{ active_route('register') }}" href="{{ route('register') }}">Register</a>
+                @endguest
+                
+                @auth
+                    <a class="nav-link {{ active_route('dashboard') }}" href="{{ route('dashboard') }}">Dashboard</a>
+                
+                    @if(Auth::user()->isCompany())
+                        <small class="text-muted">Profile</small>
+                        <a class="nav-link" href="#">My Profile</a>
+                        <a class="nav-link" href="#">Edit Profile</a>
+                        <small class="text-muted">Adverts</small>
+                        <a class="nav-link nav-link-action" href="#">Create New Advert</a>
+                        <a class="nav-link" href="#">My Adverts</a>
+                    @else
+                        <small class="text-muted">Profile</small>
+                        <a class="nav-link" href="#">My Profile</a>
+                        <a class="nav-link" href="#">Edit Profile</a>
+                        <a class="nav-link" href="#">CV Builder</a>
+                        <small class="text-muted">Find a job</small>
+                        <a class="nav-link" href="#">Search</a>
+                        <a class="nav-link" href="#">My Applications</a>
+                    @endif
                     
-                    @auth
-                        <a class="nav-link {{ active_route('dashboard') }}" href="{{ route('dashboard') }}">Dashboard</a>
-                    
-                        @if(Auth::user()->isCompany())
-                            <small class="text-muted">Profile</small>
-                            <a class="nav-link" href="#">My Profile</a>
-                            <a class="nav-link" href="#">Edit Profile</a>
-                            <small class="text-muted">Adverts</small>
-                            <a class="nav-link nav-link-action" href="#">Create New Advert</a>
-                            <a class="nav-link" href="#">My Adverts</a>
-                        @else
-                            <small class="text-muted">Profile</small>
-                            <a class="nav-link" href="#">My Profile</a>
-                            <a class="nav-link" href="#">Edit Profile</a>
-                            <a class="nav-link" href="#">CV Builder</a>
-                            <small class="text-muted">Find a job</small>
-                            <a class="nav-link" href="#">Search</a>
-                            <a class="nav-link" href="#">My Applications</a>
-                            
-                        @endif
-                        
-                    @endauth
-                </nav>
-            </div>
+                @endauth
+            </nav>
         </div>
-    
-        <div id="app">
-            <main>
-                @yield('content')
-            </main>
-        </div>
+    </div>
 
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        @yield('script')
-    </body>
-</html>
+    <div id="app">
+        <main>
+            @yield('content')
+        </main>
+    </div>
+@endsection
+@section('b_stylesheet')
+    <link href="/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    @yield('stylesheet')
+@endsection
+@section('script')
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    @yield('b_script')
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        @if (env('APP_ENV') != 'local')
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109407615-2"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-109407615-2');
+            </script>
+        @endif
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" type="image/png" href="/images/favicon.png"/>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @yield('b_stylesheet')
+    </head>
+    <body>
+        @yield('b_content')
+
+        <script src="{{ asset('js/app.js') }}"></script>
+        @yield('b_script')
+    </body>
+</html>
