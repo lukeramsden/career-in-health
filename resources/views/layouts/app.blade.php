eae4ebddb1afb23fa37674ecdbbeb31d369fec08
lukeramsden <lukeramsden@protonmail.com>
2018-04-10 10:45:06 +0100

    added animations to submenu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,42 +15,60 @@
                     <a class="nav-link {{ active_route('dashboard') }}" href="{{ route('dashboard') }}">Dashboard</a>
                 
                     @if(Auth::user()->isCompany())
+                        
+                        {{----}}
+                        
                         <small class="text-muted">Profile</small>
+                        
                         <a class="nav-link" href="#">My Profile</a>
                         <a class="nav-link" href="#">Edit Profile</a>
+                        
+                        {{----}}
+                        
                         <small class="text-muted">Adverts</small>
+                        
                         <a class="nav-link nav-link-action" href="#">Create New Advert</a>
                         <a class="nav-link" href="#">My Adverts</a>
                     @else
+                        
+                        {{----}}
+                        
                         <small class="text-muted">Profile</small>
                         
                         <a class="nav-link {{ active_route('profile.show.me') }}" href="{{ route('profile.show.me') }}">My Profile</a>
                        
-                        <a class="nav-link {{ active_route([
-                            'profile.edit',
-                            'profile.work-experience.*',
-                            'profile.references.*',
-                            'profile.certifications.*'
-                        ]) }}" href="{{ route('profile.edit') }}">Edit Profile</a>
-                        
-                        <a class="nav-link {{ active_route('cv-builder.*') }}" href="{{ route('cv-builder.profile') }}">CV Builder</a>
-                        
-                        <small class="text-muted">Find a Job</small>
                         
                         <div class="nav-section-parent">
-                            <div class="nav-section {{ active_route('search') }}">
-                                <a class="nav-link {{ active_route('search') }}" href="{{ route('search') }}">Search</a>
-                                <div class="nav-section-sub">
-                                    <a class="nav-link nav-link-sub" href="javascript:">SubMenu 1</a>
-                                    <a class="nav-link nav-link-sub {{ active_route('search') }}" href="javascript:">SubMenu 2</a>
-                                    <a class="nav-link nav-link-sub" href="javascript:">SubMenu 3</a>
+                            @php($activeRoute = active_route([
+                                                                'profile.edit',
+                                                                'profile.work-experience.*',
+                                                                'profile.references.*',
+                                                                'profile.certifications.*'
+                                                            ]))
+                            <div class="nav-section {{ $activeRoute }}">
+                                <a class="nav-link nav-section-title" onclick="navSectionClick(this)" href="javascript:">Edit Profile</a>
+                                <div class="nav-section-sub" {{ empty($activeRoute) ? 'style=display:none' : '' }}>
+                                    <a class="nav-link nav-link-sub {{ active_route('profile.edit') }}" href="{{ route('profile.edit') }}">Edit Personal Details</a>
+                                    <a class="nav-link nav-link-sub {{ active_route('profile.work-experience.*') }}" href="{{ route('profile.work-experience.edit') }}">Edit Work Experience</a>
+                                    <a class="nav-link nav-link-sub {{ active_route('profile.references.*') }}" href="{{ route('profile.references.edit') }}">Edit References</a>
+                                    <a class="nav-link nav-link-sub {{ active_route('profile.certifications.*') }}" href="{{ route('profile.certifications.edit') }}">Edit Certifications</a>
                                 </div>
                             </div>
                         </div>
+
+                        <a class="nav-link {{ active_route('cv-builder.*') }}" href="{{ route('cv-builder.profile') }}">CV Builder</a>
+                        
+                        {{----}}
+                        
+                        <small class="text-muted">Find a Job</small>
+                        
+                        <a class="nav-link {{ active_route('search') }}" href="{{ route('search') }}">Search</a>
                         
                         <a class="nav-link {{ active_route('advert.application.*') }}" href="{{ route('advert.application.index') }}">My Applications</a>
                     @endif
                   
+                    {{----}}
+                    
                     <small class="text-muted">Account</small>
                     <a class="nav-link" href="{{ route('logout.get') }}">Log Out</a>
                     
@@ -71,5 +89,11 @@
 @endsection
 @section('b_script')
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script>
+        function navSectionClick(t) {
+            $(t).next().slideToggle("slow", "easeInOutExpo");
+        }
+    </script>
     @yield('script')
 @endsection
