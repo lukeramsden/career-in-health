fbf34056f6793c2cd33d1b2e31f27dec526185fd
lukeramsden <lukeramsden@protonmail.com>
2018-04-11 10:45:02 +0100

    finished register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,10 +7,28 @@
                     <div class="col col-12 col-xl-6">
                         <form method="post" action="{{ route('register') }}">
                             {{ csrf_field() }}
+                            
+                            <div class="form-group mb-5" id="i_am">
+                                <div class="row">
+                                    <div class="col-auto">
+                                        <label class="text-muted">I am an</label>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="custom-control custom-radio">
+                                            <input type="radio" autocomplete="off" id="i_am-1" name="i_am" class="custom-control-input" value="Employer" required {{ old('i_am') == 'Employer' ? 'checked' : '' }}>
+                                            <label class="custom-control-label text-sm" for="i_am-1">Employer</label>
+                                        </div>
+                                        <div class="custom-control custom-radio">
+                                            <input type="radio" autocomplete="off" id="i_am-2" name="i_am" class="custom-control-input" value="Employee" required {{ old('i_am') == 'Employee' ? 'checked' : '' }}>
+                                            <label class="custom-control-label" for="i_am-2">Employee</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
     
                             <div class="form-group">
-                                <label>Email Address (<span class='text-action'>*</span>)</label>
-                                 <input type="email" name='email' class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                                <small class="text-muted">Email Address (<span class='text-action'>*</span>)</small>
+                                 <input required type="email" name='email' class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
                                      placeholder="Email Address" value='{{ old('email') }}'>
                                 
                                 @if ($errors->has('email'))
@@ -19,8 +37,8 @@
                             </div>
                             
                             <div class="form-group">
-                                <label>First Name (<span class='text-action'>*</span>)</label>
-                                <input name="first_name" class="form-control {{ $errors->has('first_name') ? 'is-invalid' : '' }}"
+                                <small class="text-muted">First Name (<span class='text-action'>*</span>)</small>
+                                <input required name="first_name" class="form-control {{ $errors->has('first_name') ? 'is-invalid' : '' }}"
                                     placeholder="First Name" value='{{ old('first_name') }}'>
                             
                                 @if ($errors->has('first_name'))
@@ -29,8 +47,8 @@
                             </div>
                             
                             <div class="form-group">
-                                <label>Password (<span class='text-action'>*</span>)</label>
-                                <input type="password" name='password'
+                                <small class="text-muted">Password (<span class='text-action'>*</span>)</small>
+                                <input required type="password" name='password'
                                     class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
                                     placeholder="Password">
                                 
@@ -40,11 +58,20 @@
                             </div>
                             
                             <div class="form-group">
-                                <label>Confirm Password (<span class='text-action'>*</span>)</label>
-                                <input type='password' name="password_confirmation" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="Password">
+                                <small class="text-muted">Confirm Password (<span class='text-action'>*</span>)</small>
+                                <input required type='password' name="password_confirmation" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="Password">
                                
                                 @if ($errors->has('password'))
                                     <div class="invalid-feedback">{{ $errors->first('password') }}</div>
+                                @endif
+                            </div>
+                            
+                            <div class="form-group {{ old('i_am') == 'Employer' ? '' : 'd-none' }}" id="company-name-group">
+                                <small class="text-muted">Company Name (<span class='text-action'>*</span>)</small>
+                                <input type="text" name="company_name" class="form-control {{ $errors->has('company_name') ? 'is-invalid' : '' }}" placeholder="Company Name">
+                            
+                                @if ($errors->has('company_name'))
+                                    <div class="invalid-feedback">{{ $errors->first('company_name') }}</div>
                                 @endif
                             </div>
                             
@@ -54,11 +81,22 @@
                         </form>
                     </div>
                     <div class="col col-12 col-xl-6 order-sm-first order-xl-last">
-                        <h2>Create a FREE Account</h2>
-                        <p>With over 50 years of recruitment industry experience, we offer executive search, selection and contingency recruitment services for permanent, contract and temporary requirements.</p>
+                        <div id="register-form-help-container">
+                            <div id="register-form-help-inner">
+                                <h2>Create a FREE Account</h2>
+                                <p>With over 50 years of recruitment industry experience, we offer executive search, selection and contingency recruitment services for permanent, contract and temporary requirements.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+@section('script')
+    <script>
+        $( 'input[name="i_am"]:radio' ).change(function() {
+            $('#company-name-group')[this.value === 'Employee' ? 'addClass' : 'removeClass']('d-none');
+        });
+    </script>
 @endsection
