b3636dfdbb323ab8b50fa981a4e914ed66212efa
lukeramsden <lukeramsden@protonmail.com>
2018-04-25 11:26:50 +0100

    form validation, loading icon

--- resources/views/employee/cv/edit.blade.php
+++ resources/views/employee/cv/edit.blade.php
@@ -4,13 +4,6 @@
         <cv-builder :schemas="schemas" :model="model"></cv-builder>
     </div>
 @endsection
-@section('stylesheet')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
-    
-    <style>
-
-    </style>
-@endsection
 @section('script')
     {{-- VUE TEMPLATES --}}
     
@@ -49,7 +42,7 @@
         <script type="text/x-template" id="template__cv_item">
             <div class="cv-item">
                 <template v-if="model.editing">
-                    <form @submit.prevent="model.editing = !model.editing">
+                    <form @submit.prevent="save">
                         <div v-for="(field, index) in schema.fields" :key="fieldId(field)">
                             <template v-if="_.get(field, 'type') === 'input'">
                                 <div class="form-group">
@@ -66,6 +59,7 @@
                                         v-model="model[field.model]"
                                         :id="fieldId(field)"
                                         :name="field.model"
+                                        :maxlength="field.max"
                                         :aria-describedby="fieldId(field) + 'HelpBlock'"
                                         :required="field.required">
                                 
@@ -114,7 +108,10 @@
                             </template>
                         </div>
                     
-                        <button type="button" class="btn btn-action w-25" @click="save">Save</button>
+                        <button :type="loading ? 'button' : 'submit'" class="btn btn-action w-25">
+                            <loading-icon v-if="loading" />
+                            <template v-else>Save</template>
+                        </button>
                         <button type="button" class="btn btn-link" @click="cancel">Cancel</button>
                     </form>
                 </template>
@@ -171,19 +168,22 @@
             props: ['schema', 'model'],
             methods: {
                 del (i) {
-                    axios.delete(_.get(this, 'schema.url') + '/' + _.get(this, ['model', i, 'id']))
-                        .then((response) => {
-                            if(response.status == 200)
-                                this.model.splice(i, 1);
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            alert(error);
-                        });
+                    let modelId = _.get(this, ['model', i, 'id']);
+                    
+                    // deleting real
+                    if(!!modelId) {
+                        axios.delete(_.get(this, 'schema.url') + '/' + modelId)
+                            .then((response) => {
+                                if(response.status == 200)
+                                    this.model.splice(i, 1);
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                                alert(error);
+                            });
+                    } else this.model.splice(i, 1); // deleting create form
                 },
-                add () {
-                    this.model.push({editing: true});
-                },
+                add: _.debounce(function() { this.model.push({editing:true}) }, 750, { leading: true, trailing: false }),
             },
         });
         
@@ -194,6 +194,11 @@
         Vue.component('cv-item', {
             template: '#template__cv_item',
             props: ['schema', 'model', 'index'],
+            data () {
+                return {
+                    loading: false,
+                }
+            },
             methods: {
                 //
                 save () {
@@ -218,16 +223,20 @@
                     
                     const self = this;
                     
+                    self.loading =  true;
                     if(!!modelId) { // updating existing
                         console.log('Updating model ' + modelId);
                         axios.put(_.get(this, 'schema.url') + '/' + modelId, data)
                             .then((response) => {
                                 if(response.status == 200)
                                     self.$set(self.model, 'editing', false);
+                                
+                                self.loading =  false;
                             })
                             .catch((error) => {
                                 console.log(error);
                                 alert(error);
+                                self.loading =  false;
                             });
                     } else { // creating new
                         console.log('Creating new model');
@@ -237,10 +246,13 @@
                                     self.$set(self.model, 'editing', false);
                                     self.$set(self.model, 'id', _.get(response, 'data.model.id'));
                                 }
+                                
+                                self.loading =  false;
                             })
                             .catch((error) => {
                                 console.log(error);
                                 alert(error);
+                                self.loading =  false;
                             });
                     }
                 },
@@ -260,6 +272,7 @@
                         this.$emit('delete-cv-item', this.index);
                     
                     // invert editing
+                    this.loading =  false;
                     this.$set(this.model, 'editing', false);
                 },
                 //
@@ -277,6 +290,24 @@
                 validDate: (date) => moment(date || '')._isValid,
             },
         });
+        
+        Vue.component('loading-icon', {
+            template:
+            '<div class="sk-fading-circle">' +
+                '<div class="sk-circle1 sk-circle"></div>' +
+                '<div class="sk-circle2 sk-circle"></div>' +
+                '<div class="sk-circle3 sk-circle"></div>' +
+                '<div class="sk-circle4 sk-circle"></div>' +
+                '<div class="sk-circle5 sk-circle"></div>' +
+                '<div class="sk-circle6 sk-circle"></div>' +
+                '<div class="sk-circle7 sk-circle"></div>' +
+                '<div class="sk-circle8 sk-circle"></div>' +
+                '<div class="sk-circle9 sk-circle"></div>' +
+                '<div class="sk-circle10 sk-circle"></div>' +
+                '<div class="sk-circle11 sk-circle"></div>' +
+                '<div class="sk-circle12 sk-circle"></div>' +
+            '</div>',
+        })
         
         Vue.component('date-picker', {
             template: '#template__date-picker',
@@ -307,6 +338,7 @@
                         model: 'degree',
                         helpText: 'e.g. Diploma, Bachelor\'s, PhD.',
                         required: true,
+                        max: 150,
                     },
                     {
                         type: 'input',
@@ -314,6 +346,7 @@
                         label: 'College or University',
                         model: 'school_name',
                         required: true,
+                        max: 150,
                     },
                     {
                         type: 'input',
@@ -322,6 +355,7 @@
                         model: 'field_of_study',
                         helpText: 'e.g. Management, Nursing, Psychology.',
                         required: true,
+                        max: 150,
                     },
                     {
                         type: 'input',
@@ -329,6 +363,7 @@
                         label: 'City',
                         model: 'location',
                         helpText: 'e.g. London, Manchester, Birmingham.',
+                        max: 150,
                         required: true,
                     },
                     {
@@ -348,7 +383,7 @@
                             },
                             {
                                 model: 'end_date',
-                                label: 'End Date (leave empty if you are still here TODO:placeholder)',
+                                label: 'End Date (TODO:explain empty)',
                                 inline: false,
                                 options: {
                                     format: 'MM yyyy',
@@ -415,4 +450,145 @@
             data: data,
         });
     </script>
+@endsection
+@section('stylesheet')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
+    
+    <style>
+        .sk-fading-circle {
+          margin: auto;
+          width: 25px;
+          height: 25px;
+          position: relative;
+        }
+        
+        .sk-fading-circle .sk-circle {
+          width: 100%;
+          height: 100%;
+          position: absolute;
+          left: 0;
+          top: 0;
+        }
+        
+        .sk-fading-circle .sk-circle:before {
+          content: '';
+          display: block;
+          margin: 0 auto;
+          width: 15%;
+          height: 15%;
+          background-color: #333;
+          border-radius: 100%;
+          -webkit-animation: sk-circleFadeDelay 1.2s infinite ease-in-out both;
+                  animation: sk-circleFadeDelay 1.2s infinite ease-in-out both;
+        }
+        .sk-fading-circle .sk-circle2 {
+          -webkit-transform: rotate(30deg);
+              -ms-transform: rotate(30deg);
+                  transform: rotate(30deg);
+        }
+        .sk-fading-circle .sk-circle3 {
+          -webkit-transform: rotate(60deg);
+              -ms-transform: rotate(60deg);
+                  transform: rotate(60deg);
+        }
+        .sk-fading-circle .sk-circle4 {
+          -webkit-transform: rotate(90deg);
+              -ms-transform: rotate(90deg);
+                  transform: rotate(90deg);
+        }
+        .sk-fading-circle .sk-circle5 {
+          -webkit-transform: rotate(120deg);
+              -ms-transform: rotate(120deg);
+                  transform: rotate(120deg);
+        }
+        .sk-fading-circle .sk-circle6 {
+          -webkit-transform: rotate(150deg);
+              -ms-transform: rotate(150deg);
+                  transform: rotate(150deg);
+        }
+        .sk-fading-circle .sk-circle7 {
+          -webkit-transform: rotate(180deg);
+              -ms-transform: rotate(180deg);
+                  transform: rotate(180deg);
+        }
+        .sk-fading-circle .sk-circle8 {
+          -webkit-transform: rotate(210deg);
+              -ms-transform: rotate(210deg);
+                  transform: rotate(210deg);
+        }
+        .sk-fading-circle .sk-circle9 {
+          -webkit-transform: rotate(240deg);
+              -ms-transform: rotate(240deg);
+                  transform: rotate(240deg);
+        }
+        .sk-fading-circle .sk-circle10 {
+          -webkit-transform: rotate(270deg);
+              -ms-transform: rotate(270deg);
+                  transform: rotate(270deg);
+        }
+        .sk-fading-circle .sk-circle11 {
+          -webkit-transform: rotate(300deg);
+              -ms-transform: rotate(300deg);
+                  transform: rotate(300deg);
+        }
+        .sk-fading-circle .sk-circle12 {
+          -webkit-transform: rotate(330deg);
+              -ms-transform: rotate(330deg);
+                  transform: rotate(330deg);
+        }
+        .sk-fading-circle .sk-circle2:before {
+          -webkit-animation-delay: -1.1s;
+                  animation-delay: -1.1s;
+        }
+        .sk-fading-circle .sk-circle3:before {
+          -webkit-animation-delay: -1s;
+                  animation-delay: -1s;
+        }
+        .sk-fading-circle .sk-circle4:before {
+          -webkit-animation-delay: -0.9s;
+                  animation-delay: -0.9s;
+        }
+        .sk-fading-circle .sk-circle5:before {
+          -webkit-animation-delay: -0.8s;
+                  animation-delay: -0.8s;
+        }
+        .sk-fading-circle .sk-circle6:before {
+          -webkit-animation-delay: -0.7s;
+                  animation-delay: -0.7s;
+        }
+        .sk-fading-circle .sk-circle7:before {
+          -webkit-animation-delay: -0.6s;
+                  animation-delay: -0.6s;
+        }
+        .sk-fading-circle .sk-circle8:before {
+          -webkit-animation-delay: -0.5s;
+                  animation-delay: -0.5s;
+        }
+        .sk-fading-circle .sk-circle9:before {
+          -webkit-animation-delay: -0.4s;
+                  animation-delay: -0.4s;
+        }
+        .sk-fading-circle .sk-circle10:before {
+          -webkit-animation-delay: -0.3s;
+                  animation-delay: -0.3s;
+        }
+        .sk-fading-circle .sk-circle11:before {
+          -webkit-animation-delay: -0.2s;
+                  animation-delay: -0.2s;
+        }
+        .sk-fading-circle .sk-circle12:before {
+          -webkit-animation-delay: -0.1s;
+                  animation-delay: -0.1s;
+        }
+        
+        @-webkit-keyframes sk-circleFadeDelay {
+          0%, 39%, 100% { opacity: 0; }
+          40% { opacity: 1; }
+        }
+        
+        @keyframes sk-circleFadeDelay {
+          0%, 39%, 100% { opacity: 0; }
+          40% { opacity: 1; }
+        }
+    </style>
 @endsection
