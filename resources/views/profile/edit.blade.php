0409b87383c70251dd66015dd386e952b6b2a680
lukeramsden <lukeramsden@protonmail.com>
2018-05-03 11:20:56 +0100

    improved profile edit page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,81 +1,68 @@
 @extends('layouts.app')
 @section('content')
     <div class="container my-5">
-        <div class="card card-custom">
-            <div class="card-body">
-                <form action="{{ route('profile.update') }}" method="post" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    
-                    <div class="row">
-                        <div class="col-12">
+        <form action="{{ route('profile.update') }}" method="post" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            
+            <div class="card-columns" id="profile-edit-card-columns">
+                {{--<div class="col-12 col-lg-6 mb-3">--}}
+                    <div class="card card-custom">
+                        <div class="card-body">
                             <div class="form-group">
                                 <div class="row">
-                                    <div class="col-12 col-lg-10">
+                                    <div class="col-12 offset-lg-3 col-lg-6">
+                                        <img src="{{ $profile->picture() ?? '/images/generic.png' }}" alt="Profile picture" class="img-thumbnail mx-auto d-block" style="width: 100%; max-width: 230px;">
+                                    </div>
+                                    <div class="col-12">
                                         <div class="row">
                                             <div class="col">
                                                 <label>Avatar</label>
-                                                 <div class="custom-file">
-                                                     <input type="file" class="custom-file-input {{ $errors->has('avatar') ? 'is-invalid' : '' }}" id="inputAvatar" name="avatar" accept="image/png,image/jpeg">
-                                                     <label class="custom-file-label" for="inputAvatar">Choose file...</label>
-                                                     <small class="text-muted">(.png, .jpg, .jpeg)</small>
-                                                     @if ($errors->has('avatar'))
-                                                         <div class="invalid-feedback">{{ $errors->first('avatar') }}</div>
-                                                     @endif
-                                                 </div>
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input {{ $errors->has('avatar') ? 'is-invalid' : '' }}" id="inputAvatar" name="avatar" accept="image/png,image/jpeg">
+                                                    <label class="custom-file-label" for="inputAvatar">Choose file...</label>
+                                                    <small class="text-muted">(.png, .jpg, .jpeg)</small>
+                                                    @if ($errors->has('avatar'))
+                                                        <div class="invalid-feedback">{{ $errors->first('avatar') }}</div>
+                                                    @endif
+                                                </div>
                                             </div>
                                             <div class="col-auto align-self-center">
                                                 <div class="custom-control custom-checkbox">
-                                                  <input type="checkbox" class="custom-control-input" id="inputRemoveAvatar" value="1" name="remove_avatar">
-                                                  <label class="custom-control-label" for="inputRemoveAvatar">Remove avatar?</label>
+                                                    <input type="checkbox" class="custom-control-input" id="inputRemoveAvatar" value="1" name="remove_avatar">
+                                                    <label class="custom-control-label" for="inputRemoveAvatar">Remove avatar?</label>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-12 order-first order-lg-last col-lg-2">
-                                        <img src="{{ $profile->picture() ?? '/images/generic.png' }}" alt="Profile picture" class="img-thumbnail mx-auto d-block" style="width: 100%;">
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                    
-                        <div class="col-12 col-md-6">
+                    </div>
+                {{--</div>--}}
+    
+                {{--<div class="col-12 col-lg-6 mb-3">--}}
+                    <div class="card card-custom">
+                        <div class="card-body">
                             <div class="form-group">
                                 <label>First Name (<span class="text-action">*</span>)</label>
                                 <input type="text" name="first_name" class="form-control {{ $errors->has('first_name') ? 'is-invalid' : '' }}"
                                        placeholder="First Name" value="{{ old('first_name', $profile->first_name) }}" required maxlength="40">
-                                    
+                    
                                 @if ($errors->has('first_name'))
                                     <div class="invalid-feedback">{{ $errors->first('first_name') }}</div>
                                 @endif
                             </div>
-                        </div>
-                        
-                        <div class="col-12 col-md-6">
+
                             <div class="form-group">
-                                
                                 <label>Last Name</label>
                                 <input type="text" name="last_name" class="form-control {{ $errors->has('last_name') ? 'is-invalid' : '' }}"
                                        placeholder="Last Name" value="{{ old('last_name', $profile->last_name) }}" maxlength="40">
-                                
+                    
                                 @if ($errors->has('last_name'))
                                     <div class="invalid-feedback">{{ $errors->first('last_name') }}</div>
                                 @endif
                             </div>
-                        </div>
-                        
-                        <div class="col-12 col-md-6">
-                            <div class="form-group">
-                                <label>Location</label>
-                                <input type="text" name="location" class="form-control {{ $errors->has('location') ? 'is-invalid' : '' }}"
-                                       placeholder="Location" value="{{ old('location', $profile->location) }}" maxlength="80">
                             
-                                @if ($errors->has('location'))
-                                    <div class="invalid-feedback">{{ $errors->first('location') }}</div>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <div class="col-12 col-md-6">
                             <div class="form-group">
                                 <label>Phone Number</label>
                                 <input type="tel" name="phone" class="form-control {{ $errors->has('phone') ? 'is-invalid' : ''}}"
@@ -85,54 +72,71 @@
                                     <div class="invalid-feedback">{{ $errors->first('phone') }}</div>
                                 @endif
                             </div>
+                            
+                            <div class="form-group">
+                                <label>Location</label>
+                                <input type="text" name="location" class="form-control {{ $errors->has('location') ? 'is-invalid' : '' }}"
+                                       placeholder="Location" value="{{ old('location', $profile->location) }}" maxlength="80">
+                                
+                                @if ($errors->has('location'))
+                                    <div class="invalid-feedback">{{ $errors->first('location') }}</div>
+                                @endif
+                            </div>
                         </div>
-                        
-                        <div class="col-12">
+                    </div>
+                {{--</div>--}}
+    
+                {{--<div class="col-12 mb-3">--}}
+                    <div class="card card-custom">
+                        <div class="card-body">
                             <div class="form-group">
                                 <label>Headline</label>
                                 <input type="text" name="headline" class="form-control {{ $errors->has('headline') ? 'is-invalid' : '' }}"
-                                    placeholder="Headline" value="{{ old('headline', $profile->headline) }}" maxlength="80">
-                            
+                                       placeholder="Headline" value="{{ old('headline', $profile->headline) }}" maxlength="80">
+                    
                                 @if ($errors->has('headline'))
                                     <div class="invalid-feedback">{{ $errors->first('headline') }}</div>
                                 @endif
                             </div>
-                        </div>
-                        
-                        <div class="col-12">
+                
                             <div class="form-group">
                                 <label>Description</label>
                                 <textarea name="description" class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}"
-                                    placeholder="Description" rows="12" maxlength="1000">{{ old('description', $profile->description) }}</textarea>
-    
+                                          placeholder="Description" rows="12" maxlength="1000">{{ old('description', $profile->description) }}</textarea>
+                    
                                 @if ($errors->has('description'))
                                     <div class="invalid-feedback">{{ $errors->first('description') }}</div>
                                 @endif
                             </div>
+            
                         </div>
-                        
-                        <div class="col-12">
+                    </div>
+                {{--</div>--}}
+    
+                {{--<div class="col-12 mb-3">--}}
+                    <div class="card card-custom">
+                        <div class="card-body">
                             <div class="form-group">
                                 <label>Job Roles</label>
-                                
+                    
                                 <select name="job_types[]" class="form-control job-type-control {{ $errors->has('job_types') ? 'is-invalid' : '' }}" multiple title="Jobs" size="1">
                                     @foreach(\App\JobType::all() as $job)
                                         <option {{ $profile->jobTypes->contains($job) ? 'selected' : '' }} value="{{ $job->id }}">{{ $job->name }}</option>
                                     @endforeach
                                 </select>
-    
+                    
                                 @if ($errors->has('job_types'))
                                     <div class="invalid-feedback">{{ $errors->first('job_types') }}</div>
                                 @endif
                             </div>
                         </div>
-                        
                     </div>
-                    
-                    <button type="submit" class="btn btn-action px-5 float-right">Save</button>
-                </form>
+                {{--</div>--}}
+            
             </div>
-        </div>
+            
+            <button type="submit" class="btn btn-action px-5 float-right">Save</button>
+        </form>
     </div>
 @endsection
 @section('stylesheet')
