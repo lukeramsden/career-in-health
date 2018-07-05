ac5a2cbf8292fb847159388952f1edb44949919a
lukeramsden <lukeramsden@protonmail.com>
2018-07-05 12:55:59 +0100

    code formatting

--- database/factories/AdvertFactory.php
+++ database/factories/AdvertFactory.php
@@ -3,17 +3,18 @@
 use App\Advert;
 use Faker\Generator as Faker;
 
-$factory->define(Advert::class, function (Faker $faker) {
-    $min_salary = $faker->numberBetween(0, 20000);
-    $max_salary = $faker->numberBetween($min_salary, 250000);
-    return [
-        'title' => $faker->text(120),
-        'description' => $faker->text(3000),
-        'job_role' => $faker->numberBetween(1, count(\App\JobRole::$list)),
-        'setting' => $faker->numberBetween(1, count(Advert::$settings)),
-        'type' => $faker->numberBetween(1, count(Advert::$types)),
-        'min_salary' => $min_salary,
-        'max_salary' => $max_salary,
-        'address_id' => $faker->numberBetween(1, 5),
-    ];
+$factory->define(Advert::class, function (Faker $faker)
+{
+	$min_salary = $faker->numberBetween(0, 20000);
+	$max_salary = $faker->numberBetween($min_salary, 250000);
+	return [
+		'title'       => $faker->text(120),
+		'description' => $faker->text(3000),
+		'job_role'    => $faker->numberBetween(1, count(\App\JobRole::$list)),
+		'setting'     => $faker->numberBetween(1, count(Advert::$settings)),
+		'type'        => $faker->numberBetween(1, count(Advert::$types)),
+		'min_salary'  => $min_salary,
+		'max_salary'  => $max_salary,
+		'address_id'  => $faker->numberBetween(1, 5),
+	];
 });
