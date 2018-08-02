cecac7fe8f6a2e9ed4d48c78c64fa78ec6f36e10
lukeramsden <lukeramsden@protonmail.com>
2018-08-02 14:27:18 +0100

    [advertising-space] inline svgs with .svg-inline class, as well as add CSS classes to set custom color for long form text in logo

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,2 +1,38 @@
 require('./bootstrap');
 
+/*
+ * Replace all SVG images with inline SVG
+ */
+jQuery('img.svg-inline').each(function(){
+    const $img = jQuery(this);
+    const imgID = $img.attr('id');
+    const imgClass = $img.attr('class');
+    const imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        let $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
