1625ea08b117f38e12625c54a6556e807d81f732
lukeramsden <lukeramsden@protonmail.com>
2018-08-16 15:45:57 +0100

    removed admin user creation

--- database/migrations/2018_01_02_000002_create_admins_table.php
+++ database/migrations/2018_01_02_000002_create_admins_table.php
@@ -20,21 +20,6 @@ class CreateAdminsTable extends Migration
             $table->string('avatar')->nullable();
             $table->timestamps();
         });
-
-        if(env('APP_DEBUG', false)) {
-			$user = factory(\App\User::class)
-				->create([
-					'email'             => 'admin@careerinhealth.co.uk',
-					'password'          => Hash::make('letmein'), // TODO: better password
-					'confirmed'         => 1,
-					'confirmation_code' => null,
-				]);
-			$userable = new \App\Admin();
-			$userable->first_name = 'Admin';
-			$userable->save();
-			$user->userable()->associate($userable);
-			$user->save();
-		}
     }
 
     /**
