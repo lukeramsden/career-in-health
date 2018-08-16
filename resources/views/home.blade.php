405bfec4324fe0cd6fbefc0e75df4468ea275546
lukeramsden <lukeramsden@protonmail.com>
2018-08-16 15:34:41 +0100

    pricing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,26 +6,23 @@
                 <div class="card card-custom mx-auto" style="max-width: 30rem;">
                     <img src="/images/cih-logo.svg" class="card-img-top p-5">
                     <div class="card-footer p-0">
-                        <div class="btn-group btn-group-lg btn-group-vertical btn-group-full" role="group">
+                        <div class="btn-group btn-group-lg btn-group-vertical btn-group-full btn-group-square" role="group">
                             @auth
                                 <a href="{{ route('dashboard') }}"
-                                   class="btn scale-on-hover-3 btn-action">Dashboard</a>
+                                   class="btn scale-on-hover-2 btn-action">Dashboard</a>
                             
                                 <a href="{{ route('search') }}"
-                                   class="btn scale-on-hover-3 btn-primary">Search</a>
+                                   class="btn scale-on-hover-2 btn-primary">Search</a>
                                 
                                 <a href="{{ route('logout.get') }}"
-                                   class="btn scale-on-hover-3 btn-dark-primary">Log Out</a>
+                                   class="btn scale-on-hover-2 btn-dark-primary">Log Out</a>
                             @endauth
                             @guest
                                 <a href="{{ route('login') }}"
-                                   class="btn scale-on-hover-3 btn-action">Log In</a>
+                                   class="btn scale-on-hover-2 btn-action">Log In</a>
                                 
-                                <a href="{{ route('search') }}"
-                                   class="btn scale-on-hover-3 btn-primary">Search</a>
-                                
-                                <a href="{{ route('register') }}"
-                                   class="btn scale-on-hover-3 btn-dark-primary">Register</a>
+                                <a href="{{ route('pricing') }}"
+                                   class="btn scale-on-hover-2 btn-primary">Pricing</a>
                             @endguest
                         </div>
                     </div>
---
+++ resources/views/pricing.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-12">
+                <div class="card-deck">
+                    <div class="card card-custom">
+                        <div class="card-header text-center"><h2>Silver</h2></div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><b>Unlimited</b> Adverts</li>
+                            <li class="list-group-item"><span class="text-muted">No Search Statistics</span></li>
+                            <li class="list-group-item"><span class="text-muted">No Listing Boosts</span></li>
+                            <li class="list-group-item"><b>1</b> Address</li>
+                        </ul>
+                        <div class="card-body"></div>
+                        @guest
+                            <div class="card-footer p-0">
+                                <div class="btn-group btn-group-vertical btn-group-full btn-group-square"
+                                     role="group">
+                                    <a href="{{ route('register') }}"
+                                       class="btn btn-primary">Sign Up</a>
+                                </div>
+                            </div>
+                        @endguest
+                    </div>
+                    
+                    <div class="card card-custom card-custom-action">
+                        <div class="card-header text-center"><h2>Gold</h2></div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><b class="text-action">Unlimited</b> Adverts</li>
+                            <li class="list-group-item"><b class="text-action">Detailed</b> Search Statistics</li>
+                            <li class="list-group-item"><b class="text-action">1</b> Listing Boost</li>
+                            <li class="list-group-item"><b class="text-action">3</b> Addresses</li>
+                        </ul>
+                        <div class="card-body"></div>
+                        @guest
+                            <div class="card-footer p-0">
+                                <div class="btn-group btn-group-vertical btn-group-full btn-group-square"
+                                     role="group">
+                                    <a href="{{ route('register') }}"
+                                       class="btn btn-action">Sign Up</a>
+                                </div>
+                            </div>
+                        @endguest
+                    </div>
+                    
+                    <div class="card card-custom">
+                        <div class="card-header text-center"><h2>Platinum</h2></div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><b>Unlimited</b> Adverts</li>
+                            <li class="list-group-item"><b>Detailed</b> Search Statistics</li>
+                            <li class="list-group-item"><b>3</b> Listing Boosts</li>
+                            <li class="list-group-item"><b>5</b> Addresses</li>
+                        </ul>
+                        <div class="card-body"></div>
+                        @guest
+                            <div class="card-footer p-0">
+                                <div class="btn-group btn-group-vertical btn-group-full btn-group-square"
+                                     role="group">
+                                    <a href="{{ route('register') }}"
+                                       class="btn btn-primary">Sign Up</a>
+                                </div>
+                            </div>
+                        @endguest
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
