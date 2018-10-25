e5cd306936c368db2f108e4ce0ae041af03a525b
lukeramsden <lukeramsden@protonmail.com>
2018-10-25 15:36:20 +0100

    [search-vue] almost finished now

--- resources/assets/js/components/Search.vue
+++ resources/assets/js/components/Search.vue
@@ -6,7 +6,7 @@
                     <div id="search-form">
                         <form v-on:submit.stop.prevent="">
                             <div class="form-group">
-                                <label for="what-input">What</label>
+                                <label for="what-input">What (required)</label>
                                 <input
                                     id="what-input"
                                     name="what"
@@ -20,40 +20,23 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="where-input">Where</label>
+                                <label for="where-input">Where (required)</label>
                                 <select2 name="where"
                                          id="where-input"
                                          v-model="query.where"
                                          :data="locations">
                                 </select2>
-                                <!--<select-->
-                                <!--id="where-input"-->
-                                <!--name="where"-->
-                                <!--class="custom-select">-->
-                                <!--<option-->
-                                <!--{{ old('where', Request::get('where')) != null ? '' : 'selected' }} disabled></option>-->
-                                <!--@foreach (\App\Location::getAllLocations() as $loc)-->
-                                <!--<option-->
-                                <!--{{ $loc->id == old('where', Request::get('where')) ? 'selected' : '' }} value='{{ $loc->id }}'>{{ $loc->name }}</option>-->
-                                <!--@endforeach-->
-                                <!--</select>-->
                             </div>
 
-                            <!--<div class="form-group form-group-dropdown">-->
-                            <!--<label>Radius (miles)</label>-->
-                            <!--<div id="radius-slider"></div>-->
-                            <!--<input type="hidden" name="radius" id="radius-input"-->
-                            <!--value="{{ old('radius', Request::get('radius', 50)) }}">-->
-                            <!--</div>-->
+                            <div class="form-group form-group-dropdown">
+                                <label>Radius (miles)</label>
+                                <div id="radius-slider"></div>
+                            </div>
 
-                            <!--<div class="form-group form-group-dropdown">-->
-                            <!--<label>Minimum/Maximum Salary</label>-->
-                            <!--<div id="salary-slider"></div>-->
-                            <!--<input type="hidden" name="min_salary" id="min-salary-input"-->
-                            <!--value="{{ old('min_salary', Request::get('min_salary', 0)) }}">-->
-                            <!--<input type="hidden" name="max_salary" id="max-salary-input"-->
-                            <!--value="{{ old('max_salary', Request::get('max_salary', 150000)) }}">-->
-                            <!--</div>-->
+                            <div class="form-group form-group-dropdown">
+                                <label>Minimum/Maximum Salary</label>
+                                <div id="salary-slider"></div>
+                            </div>
 
                             <!--<div class="form-group">-->
                             <!--<label>Settings</label>-->
@@ -88,62 +71,77 @@
                     </div>
                 </div>
                 <div class="col-12 col-md-7 col-lg-8" id="search-results-parent">
-                    <div class="float-right">
-                        <pagination v-model="page"
-                                    :lastPage="lastPage"></pagination>
-                    </div>
-                    <div id="search-results" v-if="resultsLoaded">
-                        <template v-for="result in results">
-                            <div class="card card-custom card-job_listing">
-                                <div class="card-body">
-                                    <a :href="route('company.show', {company: result.company.id})"
-                                       class="card-subtitle">
-                                        {{result.company.name}}
-                                        <verified-badge :company="result.company"/>
-                                    </a>
-                                    <h4 class="card-title">
-                                        {{result.job_role.name}}
-                                    </h4>
-                                    <h5>
-                                        <a :href="route('job-listing.show', {jobListing: result.id})">
-                                            {{ result.title }}
-                                        </a>
-                                    </h5>
-                                    <h6>{{ result.setting_name }}</h6>
-                                    <div id="small-details">
-                                        <div>
-                                            <p>
-                                                <span class="badge badge-secondary badge-pill p-2 px-3">
-                                                    {{ result.type_name }}
-                                                </span>
-                                            </p>
-                                        </div>
-                                        <div>
-                                            <p>
-                                                <span class="oi oi-map-marker mr-3"></span>
-                                                {{result.address.location.name}}
-                                                (<b>{{getDistanceBetween(result.address.location.lat_lng, town.lat_lng)}}
-                                                </b> miles away)
-                                            </p>
-                                        </div>
-                                        <div>
-                                            <p>
-                                                {{(result.min_salary * 100) | currency}} -
-                                                {{(result.max_salary * 100) | currency}}
-                                            </p>
-                                        </div>
-                                        <div>
-                                            <a class="btn btn-outline-primary"
-                                               :href="route('tracking.job-listing.search.click', {jobListing: result.id})">
-                                                View
+                    <div class="card card-custom card-custom-no-top-bar" v-if="!resultsLoaded || totalResults > 0">
+                        <div class="card-header" v-if="lastPage > 0">
+                            <pagination class="float-right"
+                                        v-model="page"
+                                        :disabled="!resultsLoaded"
+                                        :lastPage="lastPage"></pagination>
+                        </div>
+                        <div class="card-body p-0">
+                            <div id="search-results" v-if="resultsLoaded">
+                                <template v-for="result in results">
+                                    <div class="card card-custom card-custom-no-top-bar card-job_listing">
+                                        <div class="card-body">
+                                            <a :href="route('company.show', {company: result.company.id})"
+                                               class="card-subtitle">
+                                                {{result.company.name}}
+                                                <verified-badge :company="result.company"/>
                                             </a>
+                                            <h4 class="card-title">
+                                                {{result.job_role.name}}
+                                            </h4>
+                                            <h5>
+                                                <a :href="route('job-listing.show', {jobListing: result.id})">
+                                                    {{ result.title }}
+                                                </a>
+                                            </h5>
+                                            <h6>{{ result.setting_name }}</h6>
+                                            <div id="small-details">
+                                                <div>
+                                                    <p>
+                                                        <span class="badge badge-secondary badge-pill p-2 px-3">
+                                                            {{ result.type_name }}
+                                                        </span>
+                                                    </p>
+                                                </div>
+                                                <div>
+                                                    <p>
+                                                        <span class="oi oi-map-marker mr-3"></span>
+                                                        {{result.address.location.name}}
+                                                        (<b>{{getDistanceBetween(result.address.location.lat_lng,
+                                                        town.lat_lng)}}
+                                                    </b> miles away)
+                                                    </p>
+                                                </div>
+                                                <div>
+                                                    <p>
+                                                        {{(result.min_salary * 100) | currency}} -
+                                                        {{(result.max_salary * 100) | currency}}
+                                                    </p>
+                                                </div>
+                                                <div>
+                                                    <a class="btn btn-outline-primary"
+                                                       :href="route('tracking.job-listing.search.click', {jobListing: result.id})">
+                                                        View
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                    <hr>
+                                </template>
                             </div>
-                        </template>
+                            <loading-icon v-else></loading-icon>
+                        </div>
+                        <div class="card-footer" v-if="lastPage > 0">
+                            <pagination class="float-right"
+                                        v-model="page"
+                                        :disabled="!resultsLoaded"
+                                        :lastPage="lastPage"></pagination>
+                        </div>
                     </div>
-                    <loading-icon v-else></loading-icon>
+                    <p v-else class="text-center mt-5"><span class="font-italic text-muted">No Results</span></p>
                 </div>
             </div>
         </template>
@@ -177,6 +175,9 @@
                 query: {
                     what: '',
                     where: null,
+                    radius: null,
+                    min_salary: null,
+                    max_salary: null,
                 },
                 page: 0,
                 lastPage: 0,
@@ -189,9 +190,12 @@
 
                 town: null,
 
-                whatDropdown: null,
                 jobRoles: [],
                 locations: [],
+
+                whatDropdown: null,
+                radiusSlider: null,
+                salarySlider: null,
             };
         },
         mounted() {
@@ -204,9 +208,69 @@
                         this.whatDropdown = new Awesomplete('#what-input');
 
                         let self = this;
+
                         $what.on('awesomplete-selectcomplete', function (event) {
                             $what[0].dispatchEvent(new Event('input', {'bubbles': true}));
                             self.whatDropdown.close();
+                        });
+
+                        this.radiusSlider = document.getElementById('radius-slider');
+
+                        NoUiSlider.create(this.radiusSlider, {
+                            start: [50],
+                            step: 5,
+                            tooltips: true,
+                            range: {
+                                'min': 5,
+                                'max': 50
+                            },
+                            pips: {
+                                mode: 'steps',
+                                density: 3
+                            },
+                            format: WNumb({
+                                decimals: 0
+                            }),
+                        });
+
+                        this.radiusSlider.noUiSlider.on('change', function () {
+                            self.query.radius = parseInt(this.get());
+                        });
+
+                        $('#radius-slider').find('div.noUi-value:nth-child(47)').html('50+');
+
+                        this.salarySlider = document.getElementById('salary-slider');
+                        const salaryFormatter = WNumb({
+                            decimals: 0,
+                            thousand: ',',
+                            prefix: '£'
+                        });
+
+                        NoUiSlider.create(this.salarySlider, {
+                            start: [0, 150000],
+                            step: 500,
+                            tooltips: true,
+                            margin: 2000,
+                            range: {
+                                'min': 0,
+                                'max': 150000,
+                            },
+                            format: WNumb({
+                                decimals: 0,
+                                thousand: ',',
+                                prefix: '£',
+                            }),
+                            pips: {
+                                mode: 'positions',
+                                values: [0, 25, 50, 75, 100],
+                                density: 4,
+                                format: salaryFormatter,
+                            },
+                        });
+
+                        this.salarySlider.noUiSlider.on('change', function (values, handle) {
+                            self.query.min_salary = salaryFormatter.from(values[0]);
+                            self.query.max_salary = salaryFormatter.from(values[1]);
                         });
                     });
                 })
@@ -216,6 +280,14 @@
             $('#what-input').off();
             this.whatDropdown.destroy();
             this.whatDropdown = null;
+
+            $('#radius-slider').off();
+            this.radiusSlider.noUiSlider.destroy();
+            this.radiusSlider = null;
+
+            $('#salary-slider').off();
+            this.salarySlider.noUiSlider.destroy();
+            this.salarySlider = null;
         },
         methods: {
             route() {
@@ -296,11 +368,11 @@
                 if (args.length === 2) {
                     return calc(
                         args[0].lat, args[0].lng,
-                        args[1].lat, args[1].lng);
+                        args[1].lat, args[1].lng).toFixed(1);
                 }
                 // Option 2
                 else if (args.length === 4) {
-                    return calc(...args);
+                    return calc(...args).toFixed(1);
                 } else throw new Error('Wrong number of arguments.');
             },
         },
@@ -349,7 +421,6 @@
 
     .noUi-active .noUi-tooltip {
         display: block;
-
     }
 
     .noUi-handle {
