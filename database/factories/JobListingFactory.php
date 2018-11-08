f56eef350be02e12dcf2763356f54800fb30ead9
lukeramsden <lukeramsden@protonmail.com>
2018-11-08 10:02:18 +0000

    improved factories

--- database/factories/JobListingFactory.php
+++ database/factories/JobListingFactory.php
@@ -6,8 +6,8 @@ use Faker\Generator as Faker;
 
 $factory->define(JobListing::class, function (Faker $faker)
 {
-	$min_salary = $faker->numberBetween(5000, 20000);
-	$max_salary = $faker->numberBetween($min_salary, 250000);
+	$min_salary = $faker->numberBetween(5000, 100000);
+	$max_salary = $faker->numberBetween($min_salary, 25000);
 	$datetime   = $faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now');
 	$is_closed  = $faker->boolean(20);
 	$closed_at  = $faker->dateTimeBetween($datetime, $endDate = 'now');
@@ -22,7 +22,7 @@ $factory->define(JobListing::class, function (Faker $faker)
 		'address_id'   => $faker->numberBetween(1, Address::all()->count()),
 		'closed_at'    => $is_closed ? $closed_at : null,
 		'close_reason' => $is_closed ? $faker->text(50) : null,
-		'published'    => $faker->boolean(80),
+		'published'    => $is_closed ? true : $faker->boolean(80),
 		'created_at'   => $datetime,
 		'updated_at'   => $datetime,
 		'last_edited'  => $datetime,
